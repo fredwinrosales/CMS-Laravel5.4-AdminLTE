890b32b97b635f42bd4b2d11ef4219ce6647dcae
Fredwin Rosales <fredwinrosales@gmail.com>
2017-07-24 00:18:06 -0400

    Creando modulo Publicaciones
    Se inicia la creación de módulo crud de publicaciones de usuario.

---
+++ app/Http/Controllers/Cms/PostController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Cms;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Post $post)
+    {
+        //
+    }
+
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
